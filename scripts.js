73abf00557beffe67f914c946d97707ed0fa6ae0
Mark Fitzpatrick <markfitz815@gmail.com>
2017-11-11 19:14:22 +0000

    Initialized buildPieces function and added further Arrays

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 
 let boardArr = [];
 
+let lightPieces = [];
+let darkPieces = [];
+
 
 function buildBoard(){
 
@@ -9,21 +12,24 @@ function buildBoard(){
 
   for(var i = 1; i < 9; i++){
     for(var j = 1; j < 9; j++){
-
       if(j == 1 && firstTime == false){
         colorBool = !colorBool;
       }
       let boardSpace = {};
       boardSpace.x = i;
       boardSpace.y = String.fromCharCode(64+j);
+      boardSpace.empty = true;
       if(colorBool == true){
         boardSpace.color = "light";
         colorBool = !colorBool;
+        boardSpace.legalPlay = false;
       }else{
         boardSpace.color = "dark";
         colorBool = !colorBool;
+        boardSpace.legalPlay = true;
       }
       firstTime = false;
+      boardArr.push(boardSpace);
 
       const tileMaker = document.createElement('div');
       if(boardSpace.color == "light"){
@@ -34,16 +40,18 @@ function buildBoard(){
       }
       tileMaker.id = boardSpace.x + boardSpace.y;
       app.appendChild(tileMaker);
-
-      console.log(tileMaker.classList);
-
       console.log(boardSpace);
     }
   }
+}
+
+function buildPieces(){
+  
 }
 
 
 
 (function init(){
   buildBoard();
+  buildPieces();
 })();
