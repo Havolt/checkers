6253bdf0c0706a47901100710342a8c6371971ab
Mark Fitzpatrick <markfitz815@gmail.com>
2017-11-11 18:19:05 +0000

    Board created

--- scripts.js
+++ scripts.js
@@ -5,11 +5,14 @@ let boardArr = [];
 function buildBoard(){
 
   let colorBool = true;
-
+  let firstTime = true;
 
   for(var i = 1; i < 9; i++){
     for(var j = 1; j < 9; j++){
 
+      if(j == 1 && firstTime == false){
+        colorBool = !colorBool;
+      }
       let boardSpace = {};
       boardSpace.x = i;
       boardSpace.y = String.fromCharCode(64+j);
@@ -20,6 +23,19 @@ function buildBoard(){
         boardSpace.color = "dark";
         colorBool = !colorBool;
       }
+      firstTime = false;
+
+      const tileMaker = document.createElement('div');
+      if(boardSpace.color == "light"){
+        tileMaker.classList += "tile lightTile";
+      }
+      else{
+        tileMaker.classList += "tile darkTile";
+      }
+      tileMaker.id = boardSpace.x + boardSpace.y;
+      app.appendChild(tileMaker);
+
+      console.log(tileMaker.classList);
 
       console.log(boardSpace);
     }
