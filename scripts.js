b2d826a3730d6160d5cdca30772377c5a06cb3d4
Mark Fitzpatrick <markfitz815@gmail.com>
2017-11-11 17:59:39 +0000

    X Y and Color values added to checkerBoard object

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
-alert('hello');
+
+let boardArr = [];
+
+
+function buildBoard(){
+
+  let colorBool = true;
+
+
+  for(var i = 1; i < 9; i++){
+    for(var j = 1; j < 9; j++){
+
+      let boardSpace = {};
+      boardSpace.x = i;
+      boardSpace.y = String.fromCharCode(64+j);
+      if(colorBool == true){
+        boardSpace.color = "light";
+        colorBool = !colorBool;
+      }else{
+        boardSpace.color = "dark";
+        colorBool = !colorBool;
+      }
+
+      console.log(boardSpace);
+    }
+  }
+}
+
+
+
+(function init(){
+  buildBoard();
+})();
